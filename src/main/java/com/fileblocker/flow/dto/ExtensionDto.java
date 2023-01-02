be8d58dc6ffd36c997052cff06e7c8ae5452f84e
keeptrynjoy <sungmin4218@gmail.com>
2023-01-02 11:34:56 +0900

    [Fix] 유효성 검사 숫자만 입력가능한 사항 수정

--- src/main/java/com/fileblocker/flow/dto/ExtensionDto.java
+++ src/main/java/com/fileblocker/flow/dto/ExtensionDto.java
@@ -13,9 +13,8 @@ import javax.validation.constraints.Pattern;
 public class ExtensionDto {
 
     @NotBlank(message = "확장자를 입력해주세요.")
-    @Pattern(regexp = "[a-zA-Z]{1,20}|[a-zA-Z0-9]{1,20}", message = "영어 또는 영어와 숫자를 포함해서 1~20자리 이내로 입력해주세요.")
+    @Pattern(regexp = "^([a-zA-Z][0-9]*){1,20}$", message = "영어 또는 영어와 숫자를 포함해서 1~20자리 이내로 입력해주세요.")
     private String name;
     private String type;
     private int status;
-
 }
